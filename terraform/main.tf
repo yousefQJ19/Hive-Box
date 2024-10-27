4af02dac293aa894c65f4b4b434e945c5b839af0
yousef samara <92521652+yousefQJ19@users.noreply.github.com>
2024-10-27 16:59:46 +0200

    added integration test and teraform config files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,28 +1,5 @@
-terraform {
-  required_providers {
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.0"  # Adjust based on the provider version on your device
-    }
-  }
-}
+resource "kubernetes_manifest" "app_manifests" {
+  for_each = fileset("./manifests", "*.yaml")  # Adjust the path if your manifests are in a different folder
 
-provider "kubernetes" {
-  config_path = "${path.module}/./kubeconfig.yaml" 
-}
-
-resource "kubernetes_manifest" "deployment" {
-  manifest = file("${path.module}/manifests/deployment.yaml")
-}
-
-resource "kubernetes_manifest" "service" {
-  manifest = file("${path.module}/manifests/service.yaml")
-}
-
-resource "kubernetes_manifest" "ingress" {
-  manifest = file("${path.module}/manifests/ingress.yaml")
-}
-
-resource "kubernetes_manifest" "configmap" {
-  manifest = file("${path.module}/manifests/configmap.yaml")
+  manifest = yamldecode(file(each.value))
 }
