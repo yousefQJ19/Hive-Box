554dc90b19590c2cf570ade7cfa137eb41dd8948
yousef samara <92521652+yousefQJ19@users.noreply.github.com>
2024-10-27 20:58:34 +0200

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,13 +2,13 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.18"  # Use the correct version constraint
+      version = "~> 2.18" 
     }
   }
 }
 
 
 provider "kubernetes" {
-  config_path    = "./kubeconfig"            # Adjust path if needed
-  config_context = "kind-hive-box-cluster"  # Use the correct context name
+  config_path    = "./kubeconfig"            
+  config_context = "kind-hive-box-cluster"  
 }
